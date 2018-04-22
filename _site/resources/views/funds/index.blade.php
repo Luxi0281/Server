ef22f12ad55d374531186c5ef4959f992401a290
Luxi <portnovmaxon@mail.ru>
2018-04-22 16:36:29 +0300

    Added some scripts

--- _site/resources/views/funds/index.blade.php
+++ _site/resources/views/funds/index.blade.php
@@ -18,8 +18,24 @@
         <div class="text-center">
             <a class="btn btn-primary actionButton" href="{!! route('funds.create') !!}">Add New</a>
             <a class="btn btn-success actionButton" href="{!! url('api/funds') !!}">Show JSON</a>
-            <a class="btn btn-warning" style="width: 300px; padding: 25px; font-size: 24px; font-weight: bold">Download JSON</a>
+            <a class="btn btn-warning" style="width: 300px; padding: 25px; font-size: 24px; font-weight: bold" onclick="downloadJSON()">Download JSON</a>
         </div>
     </div>
+    <script>
+        function downloadJSON() {
+            $.getJSON("{!! url('api/funds') !!}", function(data) {
+                var jsonStr = JSON.stringify(data,  null, "\t");
+                var element = document.createElement('a');
+                element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(jsonStr));
+                element.setAttribute('download', 'funds.json');
+                element.style.display = 'none';
+                document.body.appendChild(element);
+
+                element.click();
+
+                document.body.removeChild(element);
+            });
+        }
+    </script>
 @endsection
 
--- _site/resources/views/showFund.blade.php
+++ _site/resources/views/showFund.blade.php
@@ -46,7 +46,7 @@
 </body>
 <script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="{{ URL::asset('js/scripts.js') }}" type="text/javascript"></script>
+<script src="{{ URL::asset('js/faceScripts.js') }}" type="text/javascript"></script>
 <script src="https://maps.google.com/maps/api/js"></script>
 <script>
     function regular_map() {
